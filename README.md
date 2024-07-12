915cd6f0094137bf3175ac535148ee0acc661f72
Qiujie Li <liqiujie2008@gmail.com>
2024-07-12 19:57:19 +1200

    Add tailwind css to cra app

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Add tailwind css to Create React App
 https://tailwindcss.com/docs/guides/create-react-app
 
+# All framework guides
 https://tailwindcss.com/docs/installation/framework-guides
--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-slate-200 ">
+      <div className="flex w-8/12 h-96 mx-auto align-middle justify-center  items-center">
+        <h1 className="text-3xl font-bold underline align-middle justify-center content-center">Hello TailwindCSS from CRA</h1>
+      </div>
     </div>
   );
 }
